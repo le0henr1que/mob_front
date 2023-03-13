1a785f24428830b3e58c3049c7caf4479a42be50
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-03-12 21:10:31 -0300

    feat: component -> header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,10 +1,12 @@
 
 
+//@ts-ignore
+import LogMob from "../../Assests/Frame142.svg"
 
 export function Header(){
     return (
         <div className="container-header">
-
+            <img src={LogMob} />
         </div>
     )
 }
