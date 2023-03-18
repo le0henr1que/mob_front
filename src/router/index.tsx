b8d0c4c9a1b9a5f7e2816c759481b6d1be8c6c5d
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-03-18 18:07:07 -0300

    fix: error home router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import { Home } from "../Pages/Home";
+import { Home } from "../Pages/home";
 import { NotFound } from "../Pages/NotFound";
 
 const Router = () => {
