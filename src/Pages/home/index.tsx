7d740d40631d204489831c2773f7be99726a9945
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-03-18 01:17:12 -0300

    feat: finished home

--- src/Pages/home/index.tsx
+++ src/Pages/home/index.tsx
@@ -10,6 +10,7 @@ import { Footer } from "../../Components/Footer";
 import pana from "../../Assests/pana.svg"
 //@ts-ignore
 import rafiki from "../../Assests/rafiki.svg"
+import ButtonStyle from "../../Components/Button";
 
 export function Home(){
     return (
@@ -20,21 +21,35 @@ export function Home(){
             <div className="main-card">
 
                 <div className="card">
-                    <img src={pana} />        
-                    <Text>Procure por uma local</Text>      
-                    <Text>Digite o nome de uma empresa ou estabelecimento e mostraremos a vocÊ se esses ambientes possui acessibilidade.</Text>     
-                    <Input />
-                    <Text>Não achou a empresa? Clique aqui</Text>      
-
+                    <img src={pana} />    
+                    <div className="card-title">
+                        <Text variant="font-bold title-3">Procure por uma <span>local</span></Text>    
+                    </div>  
+                    <div className="card-text">
+                        <Text variant="muted font-regular subheadline">Digite o nome de uma empresa ou estabelecimento e mostraremos a vocÊ se esses ambientes possui acessibilidade.</Text>     
+                    </div>  
+                    <div className="card-input">
+                        <Input variant="default" icon={true} placeholder="Pesquisar"/>
+                    </div>  
+                    <div className="card-text-last">
+                        <Text variant="muted font-regular body" >Não achou a empresa? <a href="#">Clique aqui</a></Text>   
+                    </div>     
                 </div>
 
                 <div className="card">
-                    <img src={rafiki} />        
-                    <Text>Procure por uma local</Text>      
-                    <Text>Digite o nome de uma empresa ou estabelecimento e mostraremos a vocÊ se esses ambientes possui acessibilidade.</Text>     
-                    <Input />
-                    <Text>Não achou a empresa? Clique aqui</Text>             
-                    
+                    <img src={rafiki} />    
+                    <div className="card-title">
+                        <Text variant="font-bold title-3">Cadastre um <span>local</span></Text>    
+                    </div>  
+                    <div className="card-text">
+                        <Text variant="muted font-regular subheadline">Quer que sua empresa seja vista como um ambiente acessível a todos? cadastre ela em nosso site!</Text>     
+                    </div>  
+                    <div className="card-buttom">
+                        <ButtonStyle variant="medium-button">Cadastre sua empresa</ButtonStyle>
+                    </div>  
+                    <div className="card-text-last">
+                        <Text variant="muted font-regular body" >Problemas para cadastrar? <a href="#">Clique aqui</a></Text>   
+                    </div>     
                 </div>
 
             </div>
